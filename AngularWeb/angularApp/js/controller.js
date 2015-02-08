245af416603c7732d281813ebb27d64f82316cdb
JimmyZhang <jx-zly@live.cn>
2015-02-08 20:48:48 +0800

    added the select vote function

--- AngularWeb/angularApp/js/controller.js
+++ AngularWeb/angularApp/js/controller.js
@@ -3,12 +3,32 @@
     $scope.channels = channelService.getList();
 }).
 controller('votesCtrl', function ($scope, $stateParams, projectService, voteItemService) {
+    $scope.isChanged = false;
     $scope.projectName = $stateParams.pn;
 
     $scope.votes = voteItemService.getList($stateParams.pid);
     $scope.project = projectService.one($stateParams.pid);
 
-    $scope.toggleSelect = function (voteItem) {
+    $scope.toggleSelect = function(voteItem) {
         voteItem.IsSelected = !voteItem.IsSelected;
-    }
+
+        if (voteItem.IsSelected != voteItem.PreSelected) {
+            $scope.isChanged = true;
+            return false;
+        }
+
+        for (var i = 0; i < $scope.votes.length; i++) {
+            if ($scope.votes[i].IsSelected != $scope.votes[i].PreSelected) {
+                $scope.isChanged = true;
+                return false;
+            }
+        }
+
+        $scope.isChanged = false;
+    };
+
+    $scope.showDetail = function(item, $event) {
+        item.showDetail = !item.showDetail;
+        $event.stopPropagation();
+    };
 });
